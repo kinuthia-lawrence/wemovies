d3836a3f7d71e2106d557a1bf2910161bddb13ae
Kinuthia Lawrence <kinuthialawrence343@gmail.com>
2024-03-06 13:14:59 +0300

    modified the index.html, style.css, images and  the script.js file

---
+++ frontend/script.js
@@ -0,0 +1,8 @@
+// 4bbc0c6716d020efceb68b68ab52d925
+const APILINK ="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=4bbc0c6716d020efceb68b68ab52d925&page=1";
+const IMG_PATH ="https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI ="https://api.themoviedb.org/3/search/movie?api_key=4bbc0c6716d020efceb68b68ab52d925&query=";
+
+const main = document.getElementById("section");
+const form = document.getElementById("form");
+const search = document.getElementById("query");
